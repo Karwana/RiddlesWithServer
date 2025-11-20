78e5577f8363a6e094607305167a2aee3cded740
Karwan Ali <karwana0000@gmail.com>
2025-11-20 10:44:11 +0100

    fixed protocol issue

--- src/RiddlesProtocol.java
+++ src/RiddlesProtocol.java
@@ -2,7 +2,7 @@ import java.net.*;
 import java.io.*;
 
 public class RiddlesProtocol {
-    private int state = 0; // 0 = waiting, 1 = asking riddle, 2 = asking another
+    private int state = 0; // 0 = waiting, 1 = asking riddle, 2 = waiting for answer
     private int currentRiddle = 0;
 
     private String[] riddles = {
@@ -23,29 +23,43 @@ public class RiddlesProtocol {
             "It increases every year."
     };
 
-
     public String processInput(String input) {
         String output = null;
 
-        if (state == 0) {
-            output = "Do you want to solve riddles? (y/n)";
-            state = 1;
-        } else if (state == 1) {
-            if (input.equalsIgnoreCase("y")) {
-                output = riddles[currentRiddle];
-            } else {
-                output = "BYE!";
-                state = 0;
-            }
-        }
-        else if (state == 2) {
-            if (input.equalsIgnoreCase(answers[currentRiddle])) {
-                output = "Correct! Do you want another riddle? (y/n)";
-                currentRiddle++;
-            } else {
-                output = "BYE!";
-            }
+        switch (state) {
+            case 0: // initial prompt
+                output = "Do you want to solve riddles? (y/n)";
+                state = 1;
+                break;
+
+            case 1: // waiting for yes/no to start or continue
+                if (input.equalsIgnoreCase("y")) {
+                    output = riddles[currentRiddle];
+                    state = 2; // now waiting for the answer
+                } else {
+                    output = "BYE!";
+                    state = 0;
+                }
+                break;
+
+            case 2: // waiting for answer
+                if (input.equalsIgnoreCase(answers[currentRiddle])) {
+                    currentRiddle++;
+                    if (currentRiddle >= riddles.length) {
+                        output = "You solved all the riddles! Bye!";
+                        state = 0;
+                        currentRiddle = 0;
+                    } else {
+                        output = "Correct! Do you want another riddle? (y/n)";
+                        state = 1; // go back to asking if user wants another
+                    }
+                } else {
+                    output = "Wrong! Here's a clue: " + clues[currentRiddle];
+                    // stay in state 2 to let user try again
+                }
+                break;
         }
 
+        return output;
     }
 }
